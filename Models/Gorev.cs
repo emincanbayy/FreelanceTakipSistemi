362a0231a861fcbe95dd22bd92d0a8e1a30120f6
emincanbayy <canbaymuhammed2@gmail.com>
2025-05-26 17:00:19 +0300

    GÖREV
    GÖREV DEKİ HATALAR ONARILDI

--- Models/Gorev.cs
+++ Models/Gorev.cs
@@ -1,41 +1,34 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
-using System.ComponentModel.DataAnnotations.Schema;
 
 namespace FreelanceTakipSistemi.Models
 {
     public class Gorev
     {
-        [Key]
         public int Id { get; set; }
 
-        [Required, StringLength(200)]
+        [Required(ErrorMessage = "Proje seçimi zorunludur.")]
+        public int? ProjeId { get; set; }
+
+        [Required(ErrorMessage = "Başlık girilmelidir.")]
+        [StringLength(100)]
         public string Baslik { get; set; }
 
-        [StringLength(2000)]
-        public string Aciklama { get; set; }
+        public string? Aciklama { get; set; }
 
-        [Required]
-        public int ProjeId { get; set; }
-
-        [ForeignKey(nameof(ProjeId))]
-        public Proje Proje { get; set; }
-
-        public ICollection<Yorum> Yorumlar { get; set; } = new List<Yorum>();
-
-        [Required]
-        public DateTime OlusturmaTarihi { get; set; } = DateTime.Now;
-
-        public DateTime? TeslimTarihi { get; set; }
-
-        [StringLength(50)]
+        [Required(ErrorMessage = "Durum seçilmelidir.")]
         public string Durum { get; set; }
 
-        [StringLength(50)]
-        public string Oncelik { get; set; }
+        [DataType(DataType.Date)]
+        public DateTime? TeslimTarihi { get; set; }
 
-        [Timestamp]
-        public byte[] RowVersion { get; set; }
+        public string? Oncelik { get; set; }
+
+        public DateTime OlusturmaTarihi { get; set; } = DateTime.Now;
+
+        public Proje? Proje { get; set; }
+
+        public ICollection<Yorum> Yorumlar { get; set; } = new List<Yorum>();
     }
 }
